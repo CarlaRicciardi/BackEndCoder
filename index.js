8e469cc848d989f5aec2bcc4dcfb6eaed4bddb4f
Carla Ricciardi <carla.ricciardi95@gmail.com>
2022-10-12 12:13:30 -0300

    Desafio 1 actualizado

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ class Usuario {
         console.log(`Nombre completo del usuario: ${nombre} ${apellido}`);
     }
 
-    addMascota(nombre) {
-        this.mascotas.push(nombre);
+    addMascota(nombreMascota) {
+        this.mascotas.push(nombreMascota);
         return this.mascotas;
     }
 
@@ -19,8 +19,8 @@ class Usuario {
         return this.mascotas.length;
     }
 
-    addBook(name, autor) {
-        this.libros.push({name, autor});
+    addBook(nombre, autor) {
+        this.libros.push({nombre: nombre, autor: autor});
         return this.libros;
     }
 
@@ -30,26 +30,12 @@ class Usuario {
     
 }
 
-const usuario = new Usuario(
-    'carla',
-    'ricciardi',
-    [
-        {
-            name: 'Harry Potter y la piedra filosofal',
-            autor: 'J. K. Rowling',
-        },
-        {
-            name: 'Los juegos del hambre',
-            autor: 'Suzanne Collins',
-        }
-    ]
-    
-    ['perro', 'gato', 'tortuga']
-)
+const usuario = new Usuario ('carla','ricciardi', [{nombre: 'Harry Potter y la piedra filosofal', autor: 'J. K. Rowling'}], ['perro', 'gato', 'tortuga'])
 
-console.log(usuario)
-console.log(usuario.getfullName());
+// console.log(usuario)
+
 console.log(usuario.addMascota("pez"));
+
+
 console.log(usuario.countMascotas());
 console.log(usuario.addBook("Las cronicas de Narnia", "C. S. Lewis"));
-console.log(usuario.getBooksNames());
