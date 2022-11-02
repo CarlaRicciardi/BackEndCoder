9c722719c502a6b74b117b6b3f5fbb646d40b1a4
Carla Ricciardi <carla.ricciardi95@gmail.com>
2022-11-02 19:23:59 -0300

    desafio4

--- classContenedor/index.js
+++ classContenedor/index.js
@@ -6,28 +6,21 @@ class Contenedor{
     }
     
     async save(objectProduct) {
-        //primero leemos el archivo
-        const data = await fs.promises.readFile('productos.json', 'utf-8')
-        //la data viene en formato texto plano, por eso la parseamos
+        const data = await fs.promises.readFile(this.file, 'utf-8')
         const dataParse = JSON.parse(data);
         
-
         const id = dataParse.length + 1
 
-        //asignamos el id al objeto
         objectProduct.id = id
 
-        //pusheamos el objeto producto que ingresamos abajo
         dataParse.push(objectProduct);
     
-
-        //tenemos que guardar dataParse pero tenemos que volver a pasarlo a texto plano
         const dataString = JSON.stringify(dataParse);
-        await fs.promises.writeFile('productos.json', dataString);
+        await fs.promises.writeFile(this.file, dataString);
         }
 
         async getById(id) {
-            const data = await fs.promises.readFile('productos.json', 'utf-8')
+            const data = await fs.promises.readFile(this.file, 'utf-8')
             const dataParse = JSON.parse(data);
             //lo parseo para convertirlo en array y asi obtener el id mediante find 
             const product = dataParse.find((product) => product.id == id);
@@ -40,29 +33,39 @@ class Contenedor{
         }
 
         async getAll() {
-            const data = await fs.promises.readFile('productos.json', 'utf-8')
+            const data = await fs.promises.readFile(this.file, 'utf-8')
             return JSON.parse(data);
 
         }
 
         async deleteById(id) {
-            const products = await this.getAll();
-            const filtradoId = products.filter(e => e.id !== id);
-            const idFind = products.find(e => e.id == id);
-                if(idFind) {
-                    const updateFile = JSON.stringify(filtradoId);
-                    fs.writeFileSync('productos.json', updateFile);
-                    console.log(`Se eliminó el objeto que tenía el id:${id}`);
-                } else {
-                    console.log(`No se encontró el objeto con id: ${id}`);
-                }
+            try{
+                const products = await this.getAll();
+                const filtradoId = products.filter(e => e.id != id);
+                await fs.promises.writeFile(this.file, JSON.stringify(filtradoId));
+            } catch(err) {
+                console.log(`No se encontró el objeto con id: ${id}`)
+            }
         }
-
 
         async deleteAll() {
-            await fs.writeFileSync('productos.json', '[]');
+            await fs.writeFileSync(this.file, '[]');
             console.log("Se han eliminado todos los productos");
         }
+
+        async update(id, title, price, thumbnail) {
+        const productos = await this.getAll();
+        const producto = productos.find((product) => product.id == id);
+
+        producto.title = title;
+        producto.price = price;
+        producto.thumbnail = thumbnail;
+
+        console.log(producto);
+        await fs.promises.writeFile(this.file, JSON.stringify(productos));
+        return producto;
+    }
+
 
     }
 
--- server.js
+++ server.js
@@ -2,29 +2,127 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 
-const Container = require ('./index');
-const fs = require('fs');
+const multer = require('multer');
 
+const Container = require ('./classContenedor/index');
+
+const {Router} = express;
+const routerProducts = Router();
+
+
+const contenedor = new Container('./data/productos.json');
+
+//middlewares
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+//router
+app.use('/api/productos', routerProducts);
+
+//form
+app.get("/form", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+//   app.post("/form", (req, res) => {
+//     const body = req.body;
+//     try{
+//         Producto.addProduct(body);
+//         res.json({success: true, error: false})
+//     }catch(err){
+//         res.json({ error:true, e:err})
+//     }
+//   });
+
+//config multer
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads');
+    },
+    filename: function (req, file, cb) {
+      cb(
+        null,
+        file.fieldname +
+          '-' +
+          Date.now() +
+          '.' +
+          file.originalname.split('.').pop()
+      );
+    },
+  });
+  const upload = multer({ storage: storage });
+
+  app.post('/uploadfile', upload.single('myFile'), (req, res) => {
+    const file = req.file;
+    if (!file) {
+      res.send({ error: true });
+    } else {
+      res.send(file);
+    }
+  });
+  
+
+
+//rutas
 app.get('/', (req,res) => {
-    res.send("<h1 style='color:black'>Bienvenidos al servidor express</h1> <a href='/productos'>productos</a>    <a href='/productoRandom'>producto random</a>")
+    res.send("<h1 style='color:red'>Bienvenidos al servidor express de CR</h1>")
 })
 
-app.get('/productos', async (req,res) => {
-    const contenedor = new Container();
+// GET '/api/productos' -> devuelve todos los productos.
+routerProducts.get('/', async (req,res) => {
     const allProducts = await contenedor.getAll();
     res.json(allProducts);
 })
 
-app.get('/productoRandom', async (req,res) => {
-    const contenedor = new Container();
-    const allProducts = await contenedor.getAll();
-    const random = allProducts[Math.floor(Math.random() * allProducts.length)];
-    res.json(random);
+// GET '/api/productos/:id' -> devuelve un producto según su id.
+routerProducts.get('/:id', async (req,res) => {
+    const {id} = req.params;
+    const prodEncontrado = await contenedor.getById(id);
+    if(prodEncontrado){
+        res.json(prodEncontrado);
+    } else {
+        res.json({error: true, msg: 'no encontrado'});
+    }
 })
+
+// POST '/api/productos' -> recibe y agrega un producto, y lo devuelve con su id asignado.
+routerProducts.post('/', async (req,res) => {
+    const {body} = req;
+    try {
+        contenedor.save(body);
+        res.send('producto guardado');    
+    } catch {
+        res.json({error: true, msg: 'No se pudo guardar el producto'});
+    }
+})
+
+// PUT '/api/productos/:id' -> recibe y actualiza un producto según su id.
+routerProducts.put('/:id', async (req,res) =>{
+    const {id} = req.params;
+    const {title, price, thumbnail} = req.body;
+    try {
+        await contenedor.update(id, title, price, thumbnail);
+        res.json({success: true})
+    } catch(e) {
+        console.log(e)
+        res.json({error: true});
+    }
+})
+
+
+// DELETE '/api/productos/:id' -> elimina un producto según su id.
+routerProducts.delete('/:id', async (req,res) => {
+    const {id} = req.params;
+    const products = await contenedor.getAll();
+
+    if(id > products.length){
+        res.json({error: true, msg: 'producto no encontrado'});
+    } else {
+        await contenedor.deleteById(id);
+        res.json({success: true, msg: 'producto eliminado'})
+    }
+})
+
 
 const server = app.listen(port, () => {
     console.log(`Server activo, escuchando en puerto http://localhost:${port}`)
 })
-
-server.on("error", error => console.log(`Error en servidor ${error}`))
-
